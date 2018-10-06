5766f47050bac11b647484d0d890ae1e567dea8f
YukhtmanReturns <34072841+YukhtmanReturns@users.noreply.github.com>
2018-10-06 00:23:01 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pcl',
+    version='01.2dev',
+    packages=['pcl','main','common'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
